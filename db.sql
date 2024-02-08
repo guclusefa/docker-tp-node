07596eedd00b56ae57627b99e62123b5a48f8e70
guclusefa <sefaaguclu@gmail.com>
2024-02-08 15:52:38 +0100

    Integration de la DB avec compose

--- db.sql
+++ db.sql
@@ -1,19 +1,17 @@
--- to create a new database
-CREATE DATABASE customersdb;
+-- Creation de la DB si elle n'existe pas
+CREATE DATABASE IF NOT EXISTS customersdb;
 
--- to use database
+-- Utilisation de la DB
 use customersdb;
 
--- creating a new table
-CREATE TABLE customer (
+-- creation de la table customer si elle n'existe pas
+CREATE TABLE IF NOT EXISTS customer (
   id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(50) NOT NULL,
   address VARCHAR(100) NOT NULL,
   phone VARCHAR(15)
 );
 
--- to show all tables
-show tables;
-
--- to describe table
-describe customer;
+-- ajout de données dans la table customer
+INSERT INTO customer (name, address, phone) VALUES ('Sefa G', 'Grenoble', '0123456789');
+INSERT INTO customer (name, address, phone) VALUES ('Ilian B', 'Grenoble', '0123456789');
--- src/db.js
+++ src/db.js
@@ -1,9 +1,8 @@
 import { createPool } from "mysql2/promise";
 
 export const pool = createPool({
-  host: "mysql-fshdujfgsdfnsdfgsj.alwaysdata.net",
-  user: "347297_user",
-  password: "KJFHSDUIFYzqejhfqhfois<duyfBHJ",
-  port: process.env.DB_PORT || 3306,
-  database: "fshdujfgsdfnsdfgsj_customers",
+  host: "db",
+  user: "user",
+  password: "password",
+  database: "customersdb"
 });
